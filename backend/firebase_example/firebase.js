ead76de69100a1133f1f95f079f8ee2878778bbc
dhananjaypahuja <pahuja.dhananjay@gmail.com>
2018-11-09 14:22:36 -0800

    Update backend/firebase_example/firebase.js
    apikeys fixed

--- backend/firebase_example/firebase.js
+++ backend/firebase_example/firebase.js
@@ -8,12 +8,12 @@ const settings = { timestampsInSnapshots: true }
 // TODO change these config settings to work with the proper database
 // NOTE this was built for a Firestore backend
 const config = {
-  apiKey: 'AIzaSyCpOr6nt-DNM_KatLfGfuX24YDQs0wX5Gg',
-  authDomain: 'gene-12015.firebaseapp.com',
-  databaseURL: 'https://gene-12015.firebaseio.com',
-  projectId: 'gene-12015',
-  storageBucket: 'gene-12015.appspot.com',
-  messagingSenderId: '1046456685060'
+  apiKey: 'AIzaSyBMVKmYH4nxqRTahVl-_M1INoiG-4abL-E',
+  authDomain: 'genetree-54b3b.firebaseapp.com',
+  databaseURL: 'https://genetree-54b3b.firebaseio.com',
+  projectId: 'genetree-54b3b',
+  storageBucket: 'genetree-54b3b.appspot.com',
+  messagingSenderId: '865253565447'
 }
 
 firebase.initializeApp(config)

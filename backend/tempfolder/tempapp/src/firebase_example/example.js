c30f541e15f6ea9c8a0adc74998de4dd042ae533
dhananjaypahuja <pahuja.dhananjay@gmail.com>
2018-11-13 11:37:15 -0800

    Clear files

--- backend/tempfolder/tempapp/src/firebase_example/example.js
+++
@@ -1,7 +0,0 @@
-import {people} from '../../backend/firebase_example'
-import { signUp, read } from '../../backend/firebase_example/people';
-
-        //create user or get existing user
-signUp("jay", "email@gmail.com", "genetree1!")
-    
-read("any@gmail.com")
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -21,3 +21,33 @@ class App extends Component {
 }
 
 export default App;
+
+// import React, { Component } from 'react';
+// import './App.css';
+// // import people from './firebase_example'
+// import {signUp }from './firebase_example/people'
+
+// class App extends Component {
+//   printMe(){
+//       signUp("jay", "email@gmail.com", "password");
+//   }
+
+//   render() {
+   
+//     return (
+//       <form>
+//           <p>Email:</p>
+//           <input type='email' name='email'></input>
+//           <p>password:</p>
+//           <input type="password" name="pass"></input>
+//           <p>Username:</p>
+//           <input type='var' name='username'></input>
+
+//           <input type="submit" value="Sign In"/>
+//     <button type='button' onClick={this.printMe} >Sign Up</button>
+//       </form>
+//     );
+//   }
+// }
+
+// export default App;
